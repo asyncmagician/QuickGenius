fb80cbc7235208765af2a2faf51646f49a720d89
PROJKT <giammarino.antony@gmail.com>
2023-06-22 23:20:45 +0200

    ✨ feat: Add HaveIBeenPwned API calls

--- quick_genius.py
+++ quick_genius.py
@@ -1,8 +1,10 @@
 import string
 import random
 import math
+import hashlib
+import requests
 
-def generate_password(length: int, with_special_chars =True, with_uppercase : bool = True, with_lowercase : bool = True, with_digits : bool = True):
+def generate_password(length: int, with_special_chars=True, with_uppercase=True, with_lowercase=True, with_digits=True):
     chars = ""
 
     if with_special_chars:
@@ -25,10 +27,15 @@ def generate_password(length: int, with_special_chars =True, with_uppercase : bo
 
     return password, entropy
 
+def check_pwned(password):
+    sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first_5_chars, rest = sha1_password[:5], sha1_password[5:]
+    response = requests.get('https://api.pwnedpasswords.com/range/' + first_5_chars)
+    return rest in response.text
+
 valid_input = False
 while not valid_input:
     length = int(input("Select the length of the desired password (8-128) ~ "))
-    
     if 8 <= length <= 128:
         with_special_chars = input("Do we include special characters ? (Y/n)").lower() or "y"
         with_uppercase = input("Do we include uppercases letters ? (Y/n)").lower() or "y"
@@ -39,7 +46,7 @@ while not valid_input:
         with_uppercase = True if with_uppercase == "y" else False
         with_lowercase = True if with_lowercase == "y" else False
         with_digits = True if with_digits == "y" else False
-        
+
         if with_special_chars or with_uppercase or with_lowercase or with_digits:
             valid_input = True
         else:
@@ -49,5 +56,10 @@ while not valid_input:
 
 password, entropy = generate_password(length, with_special_chars, with_uppercase, with_lowercase, with_digits)
 
-print(f"The generated password : {password}")
-print(f"The password entropy : {entropy} bits")
+if check_pwned(password):
+    print(f"WARNING: The password has been pwned!")
+else:
+    print(f"The password is safe.")
+
+print(f"Your generated password is {password}")
+print(f"The password entropy equal to {entropy} bits")
