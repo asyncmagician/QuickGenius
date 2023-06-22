5edd2847a93992038ec78440f9dd073f533f3526
PROJKT <giammarino.antony@gmail.com>
2023-06-22 23:33:24 +0200

    ⚡️ feat: Generate passphrase with DataMuse API

--- quick_genius.py
+++ quick_genius.py
@@ -4,6 +4,14 @@ import math
 import hashlib
 import requests
 
+def generate_passphrase(length: int):
+    response = requests.get('https://api.datamuse.com/words?max=1000&ml=planet')
+    word_library = [item['word'] for item in response.json()]   
+    passphrase = " ".join(random.choice(word_library) for _ in range(length))
+    entropy = length * math.log2(len(word_library))
+
+    return passphrase, entropy
+
 def generate_password(length: int, with_special_chars=True, with_uppercase=True, with_lowercase=True, with_digits=True):
     chars = ""
 
@@ -33,35 +41,41 @@ def check_pwned(password):
     response = requests.get('https://api.pwnedpasswords.com/range/' + first_5_chars)
     return rest in response.text
 
-valid_input = False
-while not valid_input:
-    length = int(input("Select the length of the desired password (8-128) ~ "))
-    if 8 <= length <= 128:
-        with_special_chars = input("Do we include special characters ? (Y/n)").lower() or "y"
-        with_uppercase = input("Do we include uppercases letters ? (Y/n)").lower() or "y"
-        with_lowercase = input("Do we include lowercases letters ? (Y/n)").lower() or "y"
-        with_digits = input("Do we include numbers ? (Y/n)").lower() or "y"
+valid_ptype = False
+while not valid_ptype:
+    ptype = input("Do you want to generate a (p)assword or a (pa)ssphrase? ").lower()
 
-        with_special_chars = True if with_special_chars == "y" else False
-        with_uppercase = True if with_uppercase == "y" else False
-        with_lowercase = True if with_lowercase == "y" else False
-        with_digits = True if with_digits == "y" else False
-
-        if with_special_chars or with_uppercase or with_lowercase or with_digits:
-            valid_input = True
-        else:
-            print("You must select at least one option. Please try again.")
+    if ptype in ["p", "pa"]:
+        valid_ptype = True
     else:
-        print("The length of the password should be between 8 and 128. Please try again.")
+        print("Invalid input, please enter either 'p' or 'pa'")
 
-password, entropy = generate_password(length, with_special_chars, with_uppercase, with_lowercase, with_digits)
+valid_length = False
+while not valid_length:
+    length = int(input("Select the length of the desired password or number of words in passphrase (4-128) ~ "))
+    if 4 <= length <= 128:
+        if ptype == "p":
+            with_special_chars = input("Do we include special characters ? (Y/n)").lower() or "y"
+            with_uppercase = input("Do we include uppercases letters ? (Y/n)").lower() or "y"
+            with_lowercase = input("Do we include lowercases letters ? (Y/n)").lower() or "y"
+            with_digits = input("Do we include numbers ? (Y/n)").lower() or "y"
 
-if check_pwned(password):
-    print(f"WARNING: The password has been pwned!")
-else:
-    print(f"The password is safe.")
+            with_special_chars = True if with_special_chars == "y" else False
+            with_uppercase = True if with_uppercase == "y" else False
+            with_lowercase = True if with_lowercase == "y" else False
+            with_digits = True if with_digits == "y" else False
 
-entropy_status = "high" if entropy > 80 else ("medium" if entropy > 40 else "low")
+            password, entropy = generate_password(length, with_special_chars, with_uppercase, with_lowercase, with_digits)
+            entropy_status = "high" if entropy > 80 else ("medium" if entropy > 40 else "low")
 
-print(f"Your generated password is {password}")
-print(f"The password entropy equal to {entropy} bits, current is considered {entropy_status}")
+            print(f"Your generated password is {password}")
+            print(f"The password entropy equal to {entropy} bits, which is considered {entropy_status}")
+        elif ptype == "pa":
+            passphrase, entropy = generate_passphrase(length)
+            entropy_status = "high" if entropy > 80 else ("medium" if entropy > 40 else "low")
+
+            print(f"Your generated passphrase is {passphrase}")
+            print(f"The passphrase entropy equal to {entropy} bits, which is considered {entropy_status}")
+        valid_length = True
+    else:
+        print("The length of the password or the number of words in the passphrase should be between 4 and 128. Please try again.")
